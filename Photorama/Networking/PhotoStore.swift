6d7b6da9338420c0e4dd144e1bd8f070b28d8323
JavierFo <pk.gs.xv@gmail.com>
2020-03-18 08:01:11 -0600

    ....

--- Photorama/Networking/PhotoStore.swift
+++ Photorama/Networking/PhotoStore.swift
@@ -24,12 +24,15 @@ enum PhotosResult {
 
 class PhotoStore {
     
+    let imageStore = ImageStore()
+    
     private let session: URLSession = {
         let config = URLSessionConfiguration.default
         return URLSession(configuration: config)
     }()
     
     func fetchInterestingPhotos(completion: @escaping (PhotosResult) -> Void) {
+        
         let url = FlickrAPI.interestingPhotosURL
         let request = URLRequest(url: url)
         let task = session.dataTask(with: request) {
@@ -77,12 +80,26 @@ class PhotoStore {
     }
     
     func fetchImage(for photo: Photo, completion: @escaping (ImageResult) -> Void) {
+        
+          let photoKey = photo.photoID
+            if let image = imageStore.image(forKey: photoKey) {
+                OperationQueue.main.addOperation {
+                    completion(.success(image))
+                }
+            return
+        }
+
+        
         let photoURL = photo.remoteURL
         let request = URLRequest(url: photoURL)
         let task = session.dataTask(with: request) {
             (data, response, error) -> Void in
             
             let result = self.processImageRequest(data: data, error: error)
+            
+            if case let .success(image) = result {
+                self.imageStore.setImage(image, forKey: photoKey)
+            }
             
             if let response = response as? HTTPURLResponse {
                 print("Response IMAGE HTTP Status code: \(response.statusCode)")
